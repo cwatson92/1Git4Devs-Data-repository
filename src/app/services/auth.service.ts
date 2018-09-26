ffc5dc75e3f2c6b98f1930baba058717b7b1c627
Connor <connormullett@gmail.com>
2018-09-26 14:43:59 -0400

    primed again for deployment

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,13 +6,11 @@ import { Router } from "@angular/router";
 import { Observable, Subject } from 'rxjs';
 import {APIURL } from '../../environments/environment.prod';
 
-<<<<<<< Updated upstream
-const Api_Url = "https://cashflowfinances.azurewebsites.net"
-=======
+
 // const Api_Url = "http://kcpelevennoteapie.azurewebsites.net";
 // const Api_Url = "https://kcpelevennote.azurewebsites.net"
-//const Api_Url = "https://cashflowfinances.azurewebsites.net"
->>>>>>> Stashed changes
+const Api_Url = "https://cashflowfinances.azurewebsites.net"
+
 
 
 @Injectable()
@@ -31,11 +29,8 @@ export class AuthService {
     const str = 
        `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
 
-<<<<<<< Updated upstream
-       return this._http.post(`${Api_Url}/token`,str).subscribe( (token: Token) => {
-=======
+
        return this._http.post(`${APIURL}/Token`,str).subscribe( (token: Token) => {
->>>>>>> Stashed changes
          this.userInfo = token;
          localStorage.setItem("id_token",token.access_token);
         //  localStorage.setHeader("Access-Control-Allow-Origin", "*");
